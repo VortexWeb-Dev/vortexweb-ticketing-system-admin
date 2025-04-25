b27423d6e5de8a97c959d00982af9fe75fd6269d
Rohan <rohan.2021b0131077@abes.ac.in>
2025-04-25 19:26:05 +0530

    some fixes

--- src/enums/portalOptions.js
+++ src/enums/portalOptions.js
@@ -1,20 +1,19 @@
-const portalOptions = [
-    { name: "Aghali", url: "https://aghali.bitrix24.com" },
-    { name: "Arihant", url: "https://arihantcapital.bitrix24.in" },
-    { name: "BWC", url: "https://b24-oy9apg.bitrix24.com" },
-    { name: "Springfield", url: "http://crm.springfieldproperties.ae" },
-    { name: "Livrichy", url: "https://crm.livrichy.com" },
-    { name: "The Luxury", url: "https://crm.theluxuryrealestate.ae" },
-    { name: "GI Properties", url: "http://gicrm.ae" },
-    { name: "5 Tel", url: "https://jiggle.5tel.co.uk" },
-    { name: "Mierage", url: "https://mierage.bitrix24.com" },
-    { name: "Mondus", url: "http://mondus.group" },
-    { name: "Royal Oasis", url: "https://royaloasisrealestate.bitrix24.in" },
-    { name: "Think Realty", url: "https://thinkrealtyre.ae" },
-    { name: "VServe", url: "https://vserve.bitrix24.in" },
-    { name: "X10", url: "https://x10realestate.bitrix24.com" },
-    { name: "Zealway", url: "https://zealwayproperties.bitrix24.com" },
-    { name: "Excellence", url: "https://exxcellenceambassadorsrealestate.bitrix24.com/crm/lead/list" }
-  ];
-  
-export default portalOptions
+export const CRMS = [
+  { name: "Aghali", url: "aghali.bitrix24.com" },
+  { name: "Arihant", url: "arihantcapital.bitrix24.in" },
+  { name: "BWC", url: "b24-oy9apg.bitrix24.com" },
+  { name: "Springfield", url: "crm.springfieldproperties.ae" },
+  { name: "Livrichy", url: "crm.livrichy.com" },
+  { name: "The Luxury", url: "crm.theluxuryrealestate.ae" },
+  { name: "GI Properties", url: "gicrm.ae" },
+  { name: "5 Tel", url: "jiggle.5tel.co.uk" },
+  { name: "Mierage", url: "mierage.bitrix24.com" },
+  { name: "Mondus", url: "mondus.group" },
+  { name: "Royal Oasis", url: "royaloasisrealestate.bitrix24.in" },
+  { name: "Think Realty", url: "thinkrealtyre.ae" },
+  { name: "VServe", url: "vserve.bitrix24.in" },
+  { name: "X10", url: "x10realestate.bitrix24.com" },
+  { name: "Zealway", url: "zealwayproperties.bitrix24.com" },
+  { name: "Excellence", url: "exxcellenceambassadorsrealestate.bitrix24.com/crm/lead/list" },
+  { name: "Vortex Webclouds", url: "vortexwebclouds.bitrix24.in" }
+];
