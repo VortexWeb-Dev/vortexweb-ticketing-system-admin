d638c6b7ef742e671d4a941c77e650c95eebf142
Rohan <rohan.2021b0131077@abes.ac.in>
2025-04-26 10:48:31 +0530

    removed portal url non editable

--- src/components/NewTicketModal.jsx
+++ src/components/NewTicketModal.jsx
@@ -201,20 +201,6 @@ function NewTicketModal({ onClose, setTickets, setLoading, setError }) {
 
           <form id="createTicket" onSubmit={handleSubmit} className="space-y-4">
 
-            <div className="mb-4">
-              <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
-                Portal Url:
-              </label>
-              <input
-                type="text"
-                name="title"
-                value={new URL(document.referrer).hostname}
-                // onChange={handleChange}
-                className="w-full px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-md shadow-sm bg-white dark:bg-gray-700 text-gray-900 dark:text-white focus:outline-none focus:ring-2 focus:ring-blue-500"
-                placeholder="Brief description of the issue"
-                readOnly
-              />
-            </div>
 
             <div className="mb-4">
               <label className="block text-sm font-medium text-gray-700 dark:text-gray-300 mb-1">
